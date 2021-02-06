53863f43971456c3be51253466473d8b6fbf5295
tk7424 <kagataku7424@icloud.com>
2021-02-06 21:37:10 +0900

    詳細ページも50件取得

--- pages/recipes/[id].tsx
+++ pages/recipes/[id].tsx
@@ -69,7 +69,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
     const key: any = {
       headers: {'X-API-KEY': process.env.API_KEY},
     };
-    const data = await fetch('https://konkonrecipes.microcms.io/api/v1/recipes', key)
+    const data = await fetch('https://konkonrecipes.microcms.io/api/v1/recipes?limit=50', key)
       .then(res => res.json())
       .catch(() => null);
     const paths = data.contents.map((content: any) => `/recipes/${content.id}`);
