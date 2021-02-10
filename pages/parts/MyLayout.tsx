0c7bc3ea3dc83f64832d840e3dba161f213732b6
tk7424 <kagataku7424@icloud.com>
2021-02-10 14:16:36 +0900

    head内表示テスト

--- pages/parts/MyLayout.tsx
+++ pages/parts/MyLayout.tsx
@@ -1,8 +1,10 @@
 import Header from './header'
 import Footer from './footer'
+import Head from './head'
 
 const Layout = (props: any) => (
   <>
+    <Head />
     <Header />
     {props.children}
     <Footer />
