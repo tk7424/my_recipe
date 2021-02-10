cd1a99154f3190dc81dac7d5139402945674e89a
tk7424 <kagataku7424@icloud.com>
2021-02-10 14:22:14 +0900

    no message

---
+++ pages/parts/head.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+const head = () => {
+    return(
+        <>
+        <Head>
+            <title>cuisine idee konkon 〜コンコンレシピ〜</title>
+        </Head>
+        </>
+    )
+}
+
+export default head
